133d8ef8060f05acf382f1994b003ae1cd99ad8e
Alex Biryulin <Biryulin.sanya@icloud.com>
2022-01-25 09:49:58 +0300

    CALC-4 - Started working on changin the topic from time to time

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,47 @@
 const checkbox = document.getElementById('checkbox');
 const bodyCalc = document.getElementById('calc');
+const notification = document.getElementById('notification');
+const closeNotif = document.getElementById('closeNotif')
+const btnNotif = document.getElementById('btnNotif');
 
-checkbox.addEventListener('change', () => {
+checkbox.addEventListener('change', changeTheme)
+
+
+
+setTimeout(() => {
+    notification.classList.add('open');
+}, 1000);
+
+closeNotif.onclick = () => {
+    notification.classList.remove('open');
+}
+
+btnNotif.onclick = () => {
     if (document.documentElement.hasAttribute('theme')) {
         document.documentElement.removeAttribute('theme');
     }
     else {
         document.documentElement.setAttribute('theme', 'dark');
     }
-})
+}
+
+
+
+
+function setThemeOnTime() {
+    const nowHours = new Date().getHours();
+    if (nowHours => 6 && nowHours <= 18) {
+        document.documentElement.removeAttribute('theme');
+    } else {
+        document.documentElement.setAttribute('theme', 'dark');
+    }
+}
+
+function changeTheme() {
+    if (document.documentElement.hasAttribute('theme')) {
+        document.documentElement.removeAttribute('theme');
+    }
+    else {
+        document.documentElement.setAttribute('theme', 'dark');
+    }
+}
