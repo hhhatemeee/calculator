c685b80ff618013ca572ade6d9da673f8e313be9
Alex Biryulin <Biryulin.sanya@icloud.com>
2022-02-28 13:10:47 +0300

    feature/calc-16 - update

--- src/js/windowLimit.js
+++ src/js/windowLimit.js
@@ -3,14 +3,21 @@ export default class WindowLimit {
     const windowLimit = document.createElement('div');
     const windowHeader = document.createElement('div');
     const windowInfo = document.createElement('p');
+    const windowClose = document.createElement('span');
 
-    windowLimit.append(windowHeader);
-    windowHeader.append(windowInfo);
+    windowLimit.append(windowHeader, windowInfo);
+    windowHeader.append(windowClose);
 
+    windowClose.textContent = '+';
     windowInfo.className = 'window-limit__info';
     windowHeader.className = 'window-limit__header';
     windowLimit.className = 'window-limit';
     windowLimit.id = 'windowLimit';
+
+    windowInfo.textContent = `Достигнут лимит всех сервисов. 
+    \n Сервис CC будет доступен примерно через ${60 - new Date().getMinutes()} минут. 
+    \n Сервис OE будет доступен ${29 ? `через ${29} дней.` : 'завтра.'}
+    \n Сервис FCA будет доступен примерно через ${60 - new Date().getMinutes()} минут. `
 
     return windowLimit;
   }
