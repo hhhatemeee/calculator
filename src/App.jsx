b94b3d371349dffae807d8a6023edf91c9501489
al0k1r <biryulin.sanya@icloud.com>
2022-03-23 16:36:37 +0300

    release/CALC-22 update

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,8 @@ function App(props) {
 
   const onKeyDown = (e) => setCurrentKey(e);
 
-  const onKeyDown = (e) => setCurrentKey(e);
+  const getStatusApi = () => window.convertationService.getStatusApi();
+
 
   /**
    * Theme Switching Handler
